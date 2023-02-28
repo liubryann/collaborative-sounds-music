57084d41c0a00f39ec36688f34a82da951f05e04
Bryan Liu <liubryan.developer@gmail.com>
2023-02-28 15:33:06 -0500

    chore: setup frontend configs

---
+++ frontend/components/DigitalAudioWorkstation/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./digital-audio-workstation.module.scss";
+
+export default function DigitalAudioWorkstation() {
+  return <div className={styles.test}>DigitalAudioWorkstation</div>;
+}
