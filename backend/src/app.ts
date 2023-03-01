0057220cdbc017826099658f906ee34d10f9fdb4
michaelkang01 <44816025+michaelkang01@users.noreply.github.com>
2023-02-28 23:00:18 -0500

    chore: MongoDB and ShareDB successful connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,30 @@
-import express, { Express } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import ShareDB from 'sharedb';
 import WebSocket from 'ws';
 import http from 'http';
 import WebSocketJSONStream from '@teamwork/websocket-json-stream';
+import { MongoClient, ServerApiVersion} from 'mongodb';
 
 const PORT = 3000;
-const backend = new ShareDB();
+//Maybe hide this but eh.
 
-export const app: Express = express();
+const uri = "mongodb+srv://username:chosdisiples@cluster-project.znjjqvk.mongodb.net/?retryWrites=true&w=majority";
+const options = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 };
+const client = new MongoClient(uri, options);
+let backend;
+try {
+  const db = client.connect();
+  backend = new ShareDB({db});
+  console.log("Successful database connection.")
+} catch {
+  console.error("Failed to connect to database");
+}
+
+export const app = express();
 
 const server = http.createServer(app);
 const webSocketServer = new WebSocket.Server({server:server});
-
 app.use(bodyParser.json());
 
 webSocketServer.on('connection', (webSocket) => {
