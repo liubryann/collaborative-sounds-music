e743378c0257f996cd5866fff89878a7abc39060
michaelkang01 <44816025+michaelkang01@users.noreply.github.com>
2023-03-06 21:13:01 -0500

    feat: grid loads on startup

--- frontend/components/DigitalAudioWorkstation/components/InstrumentNotes/index.tsx
+++ frontend/components/DigitalAudioWorkstation/components/InstrumentNotes/index.tsx
@@ -3,7 +3,8 @@ import styles from "./instrument-notes.module.scss";
 import * as Tone from "tone";
 import { instruments } from "../../instruments";
 import { SynthOptions } from "tone";
-import { updateSequence, updateNoteGrid } from "../../adapter";
+import { updateSequence, updateNoteGrid, getNoteGrid } from "../../adapter";
+import { connectAndSyncDoc } from "../../connection";
 
 interface InstrumentNotesProps {
   instrumentName: string;
@@ -24,9 +25,13 @@ const notes = [`C${octave}`, `D${octave}`, `E${octave}`];
 export default function InstrumentNotes({
   instrumentName,
 }: InstrumentNotesProps) {
-  const defaultNoteGrid = [...Array(notes.length)].map((x) =>
-    Array(gridLength).fill(false)
-  );
+  //will probably need to add a # value in instrument. Or make instrument names unique.
+  let defaultNoteGrid = getNoteGrid(`${instrumentName}`);
+  if (!defaultNoteGrid) {
+    defaultNoteGrid = [...Array(notes.length)].map((x) =>
+      Array(gridLength).fill(false)
+    );
+  }
 
   const gridContainerStyle = {
     display: "grid",
@@ -55,7 +60,7 @@ export default function InstrumentNotes({
   }, [instrumentName]);
 
   function toggleNoteCell(i: number, j: number) {
-    const newNoteGrid = [...noteGrid];
+    let newNoteGrid = [...noteGrid];
     newNoteGrid[i][j] = !newNoteGrid[i][j];
 
     const newSequence = [...sequence];
@@ -80,7 +85,7 @@ export default function InstrumentNotes({
     newPart.loop = true;
     setPart(newPart);
 
-    updateNoteGrid("test", newNoteGrid);
+    updateNoteGrid(`${instrumentName}`, newNoteGrid);
     setNoteGrid(newNoteGrid);
   }
 
