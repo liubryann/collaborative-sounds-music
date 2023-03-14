d5e168b3d5e3e86669e4e10c195a57ce7a4ebab9
michaelkang01 <michaelkang443@gmail.com>
2023-03-14 14:18:59 -0400

    trying weird stuff

---
+++ frontend/components/DigitalAudioWorkstation/components/AudioFile/AudioExport/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { getAudioFile } from "../../../adapter";
+
+const AudioExport = () => {
+  const clickExportAudio = () => {
+
+  }
+
+  return (
+    <div>
+      <a onClick={clickExportAudio}>Export Audio</a>
+    </div>
+  );
+}
