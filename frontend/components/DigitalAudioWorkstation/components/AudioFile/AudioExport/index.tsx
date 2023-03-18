591989bb60b75e9f68c44277eab6f9e2b4650e49
michaelkang01 <michaelkang443@gmail.com>
2023-03-18 16:39:04 -0400

    Added export options

--- frontend/components/DigitalAudioWorkstation/components/AudioFile/AudioExport/index.tsx
+++ frontend/components/DigitalAudioWorkstation/components/AudioFile/AudioExport/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styles from "./audio-export.module.scss";
 import * as Tone from "tone";
 
 export default function AudioExport() {
@@ -8,6 +9,7 @@ export default function AudioExport() {
   const [recorder, setRecorder] = useState<Tone.Recorder>();
   const [recording, setRecording] = useState<Blob>();
   const [running, setRunning] = useState<boolean>(false);
+  const [options, setOptions] = useState({filename: 'recording', extension: 'webm'});
 
   useEffect(() => {
     const newRecorder = new Tone.Recorder();
@@ -31,21 +33,40 @@ export default function AudioExport() {
     setRecording(newRecording);
   }
 
+  const nameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newOptions = {filename: e.target.value, extension: options.extension}
+    setOptions(newOptions);
+  }
+
+  const extensionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newOptions = {filename: options.filename, extension: e.target.value}
+    setOptions(newOptions);
+  }
+
   const downloadRecording = () => {
     if (!recording) {
       return;
     }
     const url = URL.createObjectURL(recording);
     const anchor = document.createElement("a");
-    anchor.download = "recording.wav";
+    anchor.download = options.filename + "." + options.extension;
     anchor.href = url;
     anchor.click();
   }
+
   return (
     <div>
       <button disabled={running} onClick={startRecording}>Start New Recording</button>
       <button disabled={!running} onClick={endRecording}>End Recording</button>
-      <button disabled={!recording} onClick={downloadRecording}>Download Recording</button>
+      <form onSubmit={downloadRecording}>
+        <input type="text" name="filename" value={options.filename} onChange={nameChange}/>
+        <select value={options.extension} onChange={extensionChange}>
+          <option value="webm">.webm (Recommended)</option>
+          <option value="wav">.wav</option>
+          <option value="mp3">.mp3</option>
+        </select>
+        <input type="submit" disabled={!recording} value="Download Recording"/>
+      </form>
     </div>
   );
 }
