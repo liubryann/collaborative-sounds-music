3dd29d1c0f98d87f97ba4a45eecaeb02020589b1
Eric <erickh.li@mail.utoronto.ca>
2023-03-29 23:47:32 -0400

    in progress

---
+++ frontend/pages/api/auth/[...auth0].ts
@@ -0,0 +1,14 @@
+import { login } from "@/services/api-service";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+
+export default handleAuth({
+  async login(req, res) {
+    try {
+      console.log(req);
+      // login();
+      await handleLogin(req, res);
+    } catch (error) {
+      res.status(error.status || 400).end(error.message);
+    }
+  },
+});
