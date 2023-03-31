543c9f7409a0492933d31c0cb1f2d06e6d8a4fb7
Eric <erickh.li@mail.utoronto.ca>
2023-03-31 14:59:22 -0400

    WIP: adding share func

---
+++ frontend/components/DigitalAudioWorkstation/components/CollaborationController/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function CollaborationController() {
+  function selectCollaborators() {}
+  return <button onClick={selectCollaborators}>Share</button>;
+}
---
+++ frontend/components/DigitalAudioWorkstation/components/CollaborationModal/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CollaborationModal() {
+  return <div>CollaborationModal</div>;
+}
