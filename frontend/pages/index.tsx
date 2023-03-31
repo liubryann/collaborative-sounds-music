5a68eb7cc3364076d2ab1d89de020306f6c52fb7
Eric <erickh.li@mail.utoronto.ca>
2023-03-31 17:25:50 -0400

    feat: styled login signup

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,15 @@
 import Login from "@/components/Registration/Login";
+import styles from "@/styles/registration.module.scss";
 
 export default function Landing() {
-  return <Login />;
+  return (
+    <div className={styles["split"]}>
+      <div className={styles["left"]}>
+        <h2>Compose something beautiful</h2>
+      </div>
+      <div className={styles["right"]}>
+        <Login />
+      </div>
+    </div>
+  );
 }
--- frontend/pages/signup/index.tsx
+++ frontend/pages/signup/index.tsx
@@ -1,6 +1,16 @@
 import Signup from "@/components/Registration/Signup";
 import React from "react";
+import styles from "@/styles/registration.module.scss";
 
 export default function SignupPage() {
-  return <Signup />;
+  return (
+    <div className={styles["split"]}>
+      <div className={styles["left"]}>
+        <h2>Begin your musical journey</h2>
+      </div>
+      <div className={styles["right"]}>
+        <Signup />
+      </div>
+    </div>
+  );
 }
