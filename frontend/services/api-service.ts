c9c96d0d377a961c723e63b071f759b0b53744ee
Eric <erickh.li@mail.utoronto.ca>
2023-04-05 13:49:54 -0400

    fix: cors error

--- frontend/services/api-service.ts
+++ frontend/services/api-service.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.API_URL || "http://localhost:3001";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 const constructURL = (path: string) => {
   return `${API_URL}${path}`;
